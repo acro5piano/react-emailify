ecbded7a2850b96711e618065c67fffb295715d6
gosho-kazuya <ketsume0211@gmail.com>
2018-10-23 15:37:09 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
+import * as juice from 'juice'
+
+const HtmlTemplate = ({ body, styles, title }: any) => `
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>${title}</title>
+      ${styles}
+    </head>
+    <body style="margin:0">
+      <div id="app">${body}</div>
+    </body>
+</html>
+`
+
+const emailify = <T extends {}>(Component: React.ComponentType<T>) => (props: T, title: string) => {
+  const sheet = new ServerStyleSheet()
+
+  const body = renderToString(sheet.collectStyles(<Component {...props} />))
+  const styles = sheet.getStyleTags()
+
+  return juice(HtmlTemplate({ body, styles, title }))
+}
+
+export default emailify
